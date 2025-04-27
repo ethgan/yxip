6ef6ce5cf5a3382598c2d6c03224f980ff9c169c
F-VJ <136722073+F-VJ@users.noreply.github.com>
2025-04-28 01:12:33 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -6,7 +6,7 @@ import os
 # 目标URL列表
 urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
         'https://ip.164746.xyz','https://stock.hostmonit.com/CloudFlareYes',
-        'https://cf.090227.xyz','https://ip.164746.xyz/'
+        'https://cf.090227.xyz','https://ip.164746.xyz'
         ]
 
 # 正则表达式用于匹配IP地址
@@ -27,6 +27,12 @@ with open('ip.txt', 'w') as file:
         
         # 根据网站的不同结构找到包含IP地址的元素
         if url == 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html':
+            elements = soup.find_all('tr')
+        if url == 'https://stock.hostmonit.com/CloudFlareYes':
+            elements = soup.find_all('tr')
+        if url == 'https://cf.090227.xyz':
+            elements = soup.find_all('tr')
+        if url == 'https://ip.164746.xyz':
             elements = soup.find_all('tr')
         elif url == 'https://ip.164746.xyz':
             elements = soup.find_all('tr')
