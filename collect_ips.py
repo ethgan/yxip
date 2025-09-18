8cbcfef912a2144b141c8a3222743a1f94986884
F-VJ <136722073+F-VJ@users.noreply.github.com>
2025-09-18 14:40:53 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -4,9 +4,8 @@ import re
 import os
 
 # 目标URL列表
-urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
-        'https://ip.164746.xyz','https://stock.hostmonit.com/CloudFlareYes',
-        'https://cf.090227.xyz','https://ip.164746.xyz'
+urls = ['https://api.uouin.com/cloudflare.html', 
+        'https://ip.164746.xyz'
         ]
 
 # 正则表达式用于匹配IP地址
@@ -26,13 +25,7 @@ with open('ip.txt', 'w') as file:
         soup = BeautifulSoup(response.text, 'html.parser')
         
         # 根据网站的不同结构找到包含IP地址的元素
-        if url == 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html':
-            elements = soup.find_all('tr')
-        if url == 'https://stock.hostmonit.com/CloudFlareYes':
-            elements = soup.find_all('tr')
-        if url == 'https://cf.090227.xyz':
-            elements = soup.find_all('tr')
-        if url == 'https://ip.164746.xyz':
+        if url == 'https://api.uouin.com/cloudflare.html':
             elements = soup.find_all('tr')
         elif url == 'https://ip.164746.xyz':
             elements = soup.find_all('tr')
